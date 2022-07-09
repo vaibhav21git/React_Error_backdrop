cfc7b4a5c50bb9b39b5fdd31effa71ae59947b72
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-07-09 20:16:03 +0530

    Adding userlist component

--- my/src/App.js
+++ my/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Addusers from './components/Users/Addusers'
+import Userlist from './components/Users/Userlist'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (<>
      <div>
       <Addusers/>
+      <Userlist users ={[]}/>
      </div>
   </> 
   )
