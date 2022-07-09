2f2441c50b9dc3f59ce7453def8b9207d07a7055
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-07-09 19:15:59 +0530

    useState of age and user added

--- my/src/components/Users/Addusers.js
+++ my/src/components/Users/Addusers.js
@@ -2,30 +2,44 @@ import React from 'react'
 import Card from '../UI/Card'
 import classes from './Addusers.module.css'
 import Button from '../UI/Button';
+import { useState } from 'react'
 
 
 function Addusers() {
 
-function adduserhandler(event){
-  event.preventDefault();
-
-}
-
-// here oue card component is a custom component it do not
-// have a method of className in it unlike div,p,form
+    const [user, setuser] = useState('');
+    const [age, setage] = useState('');
 
 
-  return (
-    <Card className  = {classes.input}>
-    <form onSubmit={adduserhandler}>
-    <label htmlFor  = "username" >Username</label>
-    <input id = "username" type = "text"/>
-    <label htmlFor  = "age" >Age (Years)</label>
-    <input id = "age" type = "number"/>
-    <Button type  = "submit">Add User</Button>
-   </form>
-   </Card>
-  )
+    function adduserhandler(event) {
+        event.preventDefault();
+        // console.log(user);
+        // console.log(age);
+    }
+
+    function userchangehandler(event) {
+       setuser(event.target.value);
+    }
+
+    function agechangehandler(event) {
+        setage(event.target.value);
+     }
+
+    // here oue card component is a custom component it do not
+    // have a method of className in it unlike div,p,form
+
+
+    return (
+        <Card className={classes.input}>
+            <form onSubmit={adduserhandler}>
+                <label htmlFor="username" >Username</label>
+                <input onChange ={userchangehandler} id="username" type="text" />
+                <label htmlFor="age" >Age (Years)</label>
+                <input  onChange ={agechangehandler} id="age" type="number" />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    )
 }
 
 export default Addusers
